ee8640e1c503c365b842d61cf94bda9200d22933
lohithnandyal <lohithnandyal1@gmail.com>
2026-03-13 21:10:27 +0530

    feat: Expand master participant list with 8 speakers

--- generate_mock_excel.py
+++ generate_mock_excel.py
@@ -2,11 +2,16 @@ import pandas as pd
 import os
 
 def create_mock_excel():
-    # ------------- EDIT YOUR EMAILS HERE -------------
+    # ------------- MASTER PARTICIPANT LIST -------------
+    # Add ALL possible participants here.
+    # Speaker_Label is the expected assignment (A, B, C...).
+    # Only people whose label appears in the transcript will be emailed.
     data = {
-        "Speaker_Label": ["Speaker A", "Speaker B", "Speaker C"],
-        "Name": ["Name A", "Name B", "Name C"],
-        "Email": ["your_first_email@company.com", "your_second_email@company.com", "your_third_email@company.com"]
+        "Speaker_Label": ["Speaker A", "Speaker B", "Speaker C", "Speaker D", "Speaker E", "Speaker F", "Speaker G", "Speaker H"],
+        "Name":          ["alex",      "jamie",     "sam",       "cassey",    "lohith",    "shreya",    "samruddhi", "gagan"     ],
+        "Email":         ["learningtraverse@gmail.com", "lohithnandyal1@gmail.com", "leematans05@gmail.com",
+                          "srujana.hm15@gmail.com", "dishahali075@gmail.com", "nandyallohith@gmail.com",
+                          "samruddhims1111@gmail.com", "shreyaanadkumar3@gmail.com"]
     }
     # -----------------------------------------------
     
